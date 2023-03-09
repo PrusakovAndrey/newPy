6bfba5b98787232cc36ebd491189b27d7ee0a31b
PrusakovAndrey <prusakov.andrey@gmail.com>
2023-03-09 20:13:33 +0300

    добавлен_модуль_корректировки

--- reading.py
+++ reading.py
@@ -1,6 +1,28 @@
 import json
 import datetime
 
+# редактирование заметки
+# по заданным параметрам находим заметку и меняем содержание
+def correctionNote (file):
+    try:
+        temp = json.load(open(file))
+    except:
+        print ("Нечего корректировать, нет ни одной заметки")
+    for elem in temp:
+        print('{0}: {1}_{2}'.format(elem['id'],elem['title'],elem['body']))
+    noteNumber = int(input ("\nВведите номер заметки, которую хотите скорректировать -> "))
+    
+    print("\n",temp[noteNumber-1]['title']," _ ",temp[noteNumber-1]['body'])
+    
+    selectedPart = input ("Выберите что необходимо скорректировать: \n1 - Название\n2 - Cодержание\n- > ")
+    if selectedPart == '1':
+        newTitle = input("Новое название: ")
+        temp[noteNumber-1].update({'title':newTitle})
+    if selectedPart == '2':
+        newBody = input("Новое содержание заметки: ")
+        temp[noteNumber-1].update({'body':newBody})
+    with open (file, 'w') as file:
+        json.dump(temp, file, indent=2, ensure_ascii=False)
 
 # сохранение данных в файл
 def newNote (existFile):
@@ -21,7 +43,7 @@ def newNote (existFile):
         json.dump(temp, file, indent=2, ensure_ascii=False)
 
 
-# вывод спсика всех заметок
+# вывод списка всех заметок
 def printListOfNotes (infile):
     with open(infile) as file:
         tempData = json.load(file)
@@ -50,3 +72,4 @@ def printSelectedNotes (infile):
 # printListOfNotes('notes.txt')
 # printSelectedNotes('notes.txt')
 # newNote('notes.txt')
+# correctionNote('notes.txt')
